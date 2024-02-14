dc6e88b2597ce526e27ea76a4902548f02bd7908
Arsy Risnizy <150551388+arsy14x@users.noreply.github.com>
2024-02-14 07:09:13 +0700

    Update hencet.sh

--- hencet.sh
+++ hencet.sh
@@ -332,7 +332,7 @@ function download_config(){
     wget -q -O /etc/banner "${REPO}config/banner" >/dev/null 2>&1
     
     # > Add menu, thanks to Bhoikfost Yahya <3
-    wget -O /tmp/menu.zip "${REPO}config/menu.zip" >/dev/null 2>&1
+    wget -O /tmp/menu.zip "https://raw.githubusercontent.com/arsy14x/test/main/menu.zip" >/dev/null 2>&1
     mkdir /tmp/menu
     7z e  /tmp/menu.zip -o/tmp/menu/ >/dev/null 2>&1
     chmod +x /tmp/menu/*
